da96518ec4c9a5bb6adbe427e470461e092e51c5
Beatriz <114697385+BeatrizPAbreu@users.noreply.github.com>
2023-08-17 09:23:15 -0300

    Update aula_07.java

--- aula_07.java
+++ aula_07.java
@@ -1,4 +1,44 @@
+//Main
 
+package engtelecom.poo;
+
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Caneta c =  new Caneta();
+//        Caneta d =  new Caneta();
+//        c.definirCor("azul");
+//        d.definirCor("verde");
+//
+//
+//        c.desenhar();
+//        d.desenhar();
+
+        Caneta[] canetas = new Caneta[5];
+
+        for (int i = 0; i < canetas.length; i++) {
+            canetas[i] = new Caneta();
+        }
+        canetas[0].definirCor("azul");
+        canetas[1].definirCor("verde");
+        canetas[2].definirCor("azul");
+        canetas[3].definirCor("preta");
+        canetas[4].definirCor("vermelha");
+
+        canetas[0].definirTinta(100);
+        canetas[0].desenhar(
+
+//        for (Caneta c : canetas) {
+//            if (c.obterCor().equals("azul")) {
+//                c.desenhar(3);
+//            }
+//        }
+
+
+    }
+}
 
 
 //Classe caneta
