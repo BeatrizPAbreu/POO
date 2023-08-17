9343744d71e9960f30ffceb8354b7b2ec9158e05
Beatriz <114697385+BeatrizPAbreu@users.noreply.github.com>
2023-08-17 09:21:48 -0300

    Update aula_07.java

--- aula_07.java
+++ aula_07.java
@@ -1,10 +1,43 @@
-import java.util.Scanner;
-
-public class App {
-
-  public static void main(String[] args) {
 
 
-  }
 
+//Classe caneta
+
+package engtelecom.poo;
+
+public class Caneta {
+
+    // Atributos
+
+    private String cor;
+    private double nivelTinta;
+
+
+    //Métodos
+    public void desenhar (int dist) {
+        System.out.println("Desenhando " + cor);
+        
+    }
+
+    public void definirCor (String c) {
+        cor = c;
+
+    }
+
+    public void definirTinta (double t) {
+        nivelTinta = t;
+    }
+
+
+    public String obterCor() {
+        return cor;
+    }
+
+    public double getNivelTinta() {
+        return nivelTinta;
+    }
+
+    public void setNivelTinta(double nivelTinta) {
+        this.nivelTinta = nivelTinta;
+    }
 }
